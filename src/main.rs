6ff7496895920e139d120961add1c1ac6083ffff
Swastik <swastik.patro02@gmail.com>
2026-03-06 17:09:28 +0530

    fetched in Vec format

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 
 //`?` only works if your function returns a `Result`
 
-// use serde::Deserialize;
 #[tokio::main]
-// #[derive(Debug,Deserialize)]
 async fn main()->Result<(),Box<dyn std::error::Error>>{
-    let body = reqwest::get("https://hacker-news.firebaseio.com/v0/topstories.json").await?;
-    println!("string body :{:?}", body.text().await?);
-    // println!("json type body:{}", body.json().await?);   //cant cause derive(Deserialize) is only on structs, enums
+    let json_body: Vec<u64>= reqwest::get("https://hacker-news.firebaseio.com/v0/topstories.json").await?.json().await?;
+    println!("string body :{:?}", json_body);
+    println!("!st 10 ids:{:?}",&json_body[..10]);
+    println!("!st 10 ids:{:?}",&json_body[..10].to_vec());
+
     Ok(())
 }
